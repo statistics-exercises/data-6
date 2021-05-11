1c057d3b5a238767c98b5d969f7aab795957e1ea
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 17:22:25 +0100

    Added answer

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import numpy as np
 x = np.loadtxt("data.dat")
 
 # Your code will go here
-
-
+x.sort()
+y = np.zeros( len(x) )
+for i in range(len(x)) : y[i] = (i+1)/len(x)
 
 # This will produce the final graph
 plt.plot( x, y, 'k-' )
+plt.xlabel("x")
+plt.ylabel("cumulative distribution")
 plt.savefig("cumulative-pdf.png")
